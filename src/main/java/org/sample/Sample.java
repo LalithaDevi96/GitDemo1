80e2c0954643107ef1747e5fe94e19f12ef13035
LalithaDevi96 <lalithadevi96@gmail.com>
2024-03-13 21:38:17 +0530

    Git Demo 1

---
+++ src/main/java/org/sample/Sample.java
@@ -0,0 +1,18 @@
+package org.sample;
+
+public class Sample {
+	
+	public static void main(String[] args) {
+		
+		//Array declaration
+		//type 1:
+		int a[]= {1,2};
+		//type 2:
+		int b[];
+		b = new int[10];
+		//type 3:
+		int c[] = new int[] {1,2,3};
+		
+	}
+
+}
